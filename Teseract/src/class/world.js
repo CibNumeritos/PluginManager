737ca1fc75c09e34452937c07d97983000c1eb24
CibNumeritos <85264290+CibNumeritos@users.noreply.github.com>
2022-07-20 20:58:10 -0500

    Added setTimeout and setInterval functions to the native World class.

--- Teseract/src/class/world.js
+++ Teseract/src/class/world.js
@@ -14,6 +14,68 @@ World.prototype.registerCommand = function (name, description, aliases, permissi
 
 World.prototype.getScore = function () { };
 
-World.prototype.setTimeout = function () { };
+/**
+ * @remarks Sets a tick-based interval.
+ * @param {() => void} callback Callback to call until the delay finish.
+ * @param {number} tick Time (in ticks) until the callback is called.
+ */
+World.prototype.setTimeout = function (callback, tick) {
 
-World.prototype.setInterval = function () { };
+    let current_tick = 0
+    let loop = true;
+    const tick_event = world.events.tick.subscribe((data) => {
+
+        if (current_tick === 0) current_tick = data.currentTick + tick
+
+        try {
+
+            if (current_tick <= data.currentTick) {
+
+                callback();
+
+                world.events.tick.unsubscribe(tick_event);
+
+            };
+
+        } catch (error) {
+
+            console.warn(`${error} : ${error.stack}`);
+
+        };
+
+    });
+
+};
+
+/**
+ * @remarks Sets a tick-based interval.
+ * @param {() => void} callback Callback to call until the delay finish.
+ * @param {number} tick Time (in ticks) until the callback is called.
+ */
+World.prototype.setInterval = function (callback, tick) {
+
+    let current_tick = 0
+    let loop = true;
+    const tick_event = world.events.tick.subscribe((data) => {
+
+        if (current_tick === 0) current_tick = data.currentTick + tick
+
+        try {
+
+            if (current_tick <= data.currentTick) {
+
+                callback();
+
+                current_tick = data.currentTick + tick;
+
+            };
+
+        } catch (error) {
+
+            console.warn(`${error} : ${error.stack}`);
+
+        };
+
+    });
+
+};
